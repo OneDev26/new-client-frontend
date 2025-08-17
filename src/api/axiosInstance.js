3e4f9c18398299beb988cad551fd4b5e31128889
Rudra299792 <shivam.tomar31415@gmail.com>
2025-08-18 03:19:41 +0530

    first commit

--- src/api/axiosInstance.js
+++ src/api/axiosInstance.js
@@ -8,7 +8,7 @@ export const setLogoutHandler = (handler) => {
 };
 
 const axiosInstance = axios.create({
-  baseURL: 'https://server.survill.com',
+  baseURL: 'http://127.0.0.1:8000',
 });
 
 axiosInstance.interceptors.request.use((config) => {
